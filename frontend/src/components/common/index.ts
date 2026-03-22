3934198950567604129e4c6bae20fd96cd0d4be9
Isac <iscedher@gmail.com>
2026-03-22 02:15:06 -0600

    correccion de error en index.ts

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -2,6 +2,6 @@ export { default as ComponentCard } from './ComponentCard';
 export { default as PageMeta, AppWrapper } from './PageMeta';
 export { default as PageBreadcrumb } from './PageBreadCrumb';
 export { default as GridShape } from './GridShape';
-export { default as ScrollToTop } from './ScrollToTop';
-export { default as ThemeToggleButton } from './ThemeToggleButton';
+export { ScrollToTop } from './ScrollToTop';
+export { ThemeToggleButton } from './ThemeToggleButton';
 export { default as ThemeTogglerTwo } from './ThemeTogglerTwo';
